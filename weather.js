027711d2ab7f9fadfbf6e3cd199d7319c7c078cc
ingielek <ingielewicz.p@gmail.com>
2018-01-07 21:29:02 +0100

    next commit

--- weather.js
+++ weather.js
@@ -1,16 +1,48 @@
 const https = require('https');
+const http = require('http');
 const api = require('./api.json');
+function printWeather(weather) {
+    const message = `Current temperature in ${weather.location.city} is ${weather.current_observation.temp_c}C`;
+    console.log(message);
+}
+
+
+function printError(error) {
+    console.error(error.message);
+}
 
 function get(query) {
-    const request = https.get(`https://api.wunderground.com/api/${api.key}/geolookup/conditions/q/${query}.json `,
-        response => {
-        let body = "";
-        response.on('data', chunk => {
-            body += chunk;
-        });
-        response.on('end', () => {
-            console.log(body);
-        });
-    });
+    const readableQuery = query.replace('_', ' ');
+    try {
+        const request = https.get(`https://api.wunderground.com/api/${api.key}/geolookup/conditions/q/${query}.json`, response => {
+            if (response.statusCode === 200) {
+                let body = "";
+                response.on('data', chunk => {
+                    body += chunk;
+                });
+                response.on('end', () => {
+                    try {
+                        const weather = JSON.parse(body);
+                        if (weather.location) {
+                            printWeather(weather);
+                        } else {
+                            const queryError = new Error(`The location "${readableQuery}" was not found.`);
+                            printError(queryError);
+                        }
+                    } catch (error) {
+                        printError(error);
+                    }
+                });
+            } else {
+                const statusCodeError = new Error(`There was an error getting the message for ${readableQuery}. (${http.STATUS_CODES[response.statusCode]})`);
+                printError(statusCodeError);
+            }
+            });
+        request.on("error", printError);
+    } catch (error) {
+
+        printError(error);
+    }
 }
+
 module.exports.get = get;
